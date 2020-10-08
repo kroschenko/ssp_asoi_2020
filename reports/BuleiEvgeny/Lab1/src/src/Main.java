08a156acea32b39a5adfa0f3b0047f3e262cfca1
BuleiZhenya <55019478+AS005003@users.noreply.github.com>
2020-10-08 10:52:01 +0300

    Add files via upload

--- reports/BuleiEvgeny/Lab1/src/src/Main.java
+++ reports/BuleiEvgeny/Lab1/src/src/Main.java
@@ -2,7 +2,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main( String[] args) {
-        function1(new double[]{3,8,1,5,9,7,2});
+        double[] arr = new double[args.length];
+        for(int i = 0; i<args.length; i++)
+            arr[i]=Double.parseDouble(args[i]);
+        function1(arr);
         reverse(new double[]{3,8,1,5,9,7,2});
         if(polindrome("А лис, он умён - крыса сыр к нему носила.")) {
             System.out.println("this is polindrome");
@@ -10,13 +13,15 @@ public class Main {
 
     }
     public static void function1(double[] arr){
-        double max, min;
-        max = min = arr[0];
+        double max, min,sum,proiz;
+        max = min =sum=proiz= arr[0];
         for(int i=1;i<arr.length;i++){
             if(max<arr[i]) max=arr[i];
             if(min>arr[i]) min=arr[i];
+            sum=sum+arr[i];
+            proiz=proiz*arr[i];
         }
-        System.out.println("max="+max+"\nmin="+min+"\nsum="+(max+min)+"\ncomp="+(max*min));
+        System.out.println("max="+max+"\nmin="+min+"\nsum="+sum+"\ncomp="+proiz);
     }
     public static void reverse(double[] arr){
         double[] newArr = new double[arr.length];
@@ -26,6 +31,7 @@ public class Main {
         System.out.print("Reserve array ");
         for(int z=0;z<newArr.length;z++)
             System.out.print(" "+newArr[z]);
+        System.out.println();
     }
 
     public static boolean polindrome(String str) {
